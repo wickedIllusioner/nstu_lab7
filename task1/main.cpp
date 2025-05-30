e5643e400ea14f87a8258ed4a502145bf3bb3647
WickedIllusioner <xxxvanyalive228xxx@gmail.com>
2025-05-30 09:24:28 +0700

    update task 1

--- task1/main.cpp
+++ task1/main.cpp
@@ -3,13 +3,6 @@
 #include <cmath>
 
 
-void drawSquare(sf::RenderWindow& window, sf::Vector2f position, double size, sf::Color color) {
-    sf::RectangleShape square(sf::Vector2f(size, size));
-    square.setPosition(position);
-    square.setFillColor(color);
-    window.draw(square);
-}
-
 void drawPythagoreanTree(sf::RenderWindow& window, sf::Vector2f position, double size, double angle, int depth, int color) {
     if (depth == 0) {
         return;
@@ -17,48 +10,51 @@ void drawPythagoreanTree(sf::RenderWindow& window, sf::Vector2f position, double
 
     sf::RectangleShape square(sf::Vector2f(size, size));
     square.setOutlineColor(sf::Color::Green);
-    square.setOutlineThickness(0.3);
+    square.setOutlineThickness(0.5);
     square.setFillColor(sf::Color(color, 240, 0));
     square.setOrigin(size / 2, size / 2);
     square.setPosition(position);
     square.setRotation(angle);
     window.draw(square);
+    
+    double radAngle = angle * M_PI / 180.0;
 
-    // Calculate the positions for the subsequent squares
-    // We need to find the top-center of the current square to place the triangle
-    sf::Vector2f topCenter;
-    topCenter.x = position.x + (size / 2) * sin(angle * M_PI / 180);
-    topCenter.y = position.y - (size / 2) * cos(angle * M_PI / 180);
+    // Координаты верхней левой вершины текущего квадрата
+    sf::Vector2f topLeft;
+    topLeft.x = -size / 2.0;
+    topLeft.y = -size;
 
-    // The new squares will have side length (size / sqrt(2))
-    double newSize = size / sqrt(2.);
+    // Координаты верхней правой вершины текущего квадрата
+    sf::Vector2f topRight;
+    topRight.x = size / 2.0;
+    topRight.y = -size;
 
+    // Поворачиваем эти точки на текущий угол и смещаем их к position
+    sf::Vector2f topLeftRotated;
+    topLeftRotated.x = position.x + (topLeft.x * cos(radAngle) - topLeft.y * sin(radAngle));
+    topLeftRotated.y = position.y + (topLeft.x * sin(radAngle) + topLeft.y * cos(radAngle));
 
-    // Нахождение координат левого квадрата
-    sf::Vector2f leftSquarePos;
-    leftSquarePos.x = topCenter.x - (newSize / 2) * cos(angle * M_PI / 180);
-    leftSquarePos.y = topCenter.y - (newSize / 2) * sin(angle * M_PI / 180);
+    sf::Vector2f topRightRotated;
+    topRightRotated.x = position.x + (topRight.x * cos(radAngle) - topRight.y * sin(radAngle));
+    topRightRotated.y = position.y + (topRight.x * sin(radAngle) + topRight.y * cos(radAngle));
+    
+    // Длина стороны следующих квадратов
+    double newSize = size / std::sqrt(2.0);
 
-    // Нахождение координат правого квадрата
-    sf::Vector2f rightSquarePos;
-    rightSquarePos.x = topCenter.x + (newSize / 2) * cos(angle * M_PI / 180);
-    rightSquarePos.y = topCenter.y + (newSize / 2) * sin(angle * M_PI / 180);
-
-    // Рекурсивный вызов функции для генерации двух квадратов
-    drawPythagoreanTree(window, leftSquarePos, newSize, angle - 45, depth - 1, color - 25);
-    drawPythagoreanTree(window, rightSquarePos, newSize, angle + 45, depth - 1, color - 25);
+    // Рекурсивные вызовы для двух новых ветвей
+    drawPythagoreanTree(window, topLeftRotated, newSize, angle - 45, depth - 1, color - 20);
+    drawPythagoreanTree(window, topRightRotated, newSize, angle + 45, depth - 1, color - 20);
 }
 
+
 int main() {
-    int maxDepth {10};
-    // std::cout << "Введите число глубины рекурсии: ";
-    // std::cin >> maxDepth;
+    int maxDepth {12};
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Дерево Пифагора");
     window.setFramerateLimit(60);
 
-    sf::Vector2f initialPosition(400, 500); // Bottom-center of the window
-    double initialSize = 100.0f;             // Size of the base square
+    sf::Vector2f initialPosition(400, 500);
+    double initialSize = 100;
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
